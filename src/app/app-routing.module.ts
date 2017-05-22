8212a2b0d4ba39664f4702a73b803e744fa5a709
gritman <gritman@126.com>
2017-05-22 22:24:50 +0800

    路由的基本使用:routes routerLink router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
   {
-    path: '',
-    children: []
+    path: '', // 注意path不能以/开头
+    component: HomeComponent
+  },
+  {
+    path: 'product', // 这是根路由,不是子路由
+    component: ProductComponent
   }
 ];
 
