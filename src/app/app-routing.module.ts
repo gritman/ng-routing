902a7d64e579154d8448579609cde358f831dcab
gritman <gritman@126.com>
2017-05-24 13:16:59 +0800

    在查询参数时传递数据

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
+import { Code404Component } from './code404/code404.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'product', // 这是根路由,不是子路由
     component: ProductComponent
+  },
+  {
+    path: '**', // 路由表是按先后顺序匹配的,所以通配路由必须放在最后面
+    component: Code404Component
   }
 ];
 
