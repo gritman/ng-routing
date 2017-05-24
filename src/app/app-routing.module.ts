fbeccefadcf11245f3fa5d862fb7228dbd79693a
gritman <gritman@126.com>
2017-05-24 23:18:24 +0800

    子路由的配置和使用

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
+import { ProductDescComponent } from './product-desc/product-desc.component';
+import { SellerInfoComponent } from './seller-info/seller-info.component';
 import { Code404Component } from './code404/code404.component';
 
 const routes: Routes = [
@@ -16,7 +18,17 @@ const routes: Routes = [
   },
   {
     path: 'product/:id', // 这是根路由,不是子路由
-    component: ProductComponent
+    component: ProductComponent,
+    children: [
+      {
+        path: '', // 这是子路由
+        component: ProductDescComponent
+      },
+      {
+        path: 'seller/:id',
+        component: SellerInfoComponent
+      }
+    ]
   },
   {
     path: '**', // 路由表是按先后顺序匹配的,所以通配路由必须放在最后面
